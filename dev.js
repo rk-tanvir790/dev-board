bb06ffc144f6b23a8fb41e41991fc3aa8f20dc6a
rk-tanvir790 <ktanvir790@gmail.com>
2025-03-27 05:54:25 +0600

    completed navbar section & added google font and custom css

---
+++ dev.js
@@ -0,0 +1,30 @@
+// function for button. this button will change body color randomly
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+const button = document.getElementById('color-btn');
+
+button.addEventListener('click', function() {
+    document.body.style.backgroundColor = getRandomColor();
+
+    
+        // const color1 = getRandomColor();
+        // const color2 = getRandomColor();
+        // document.body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+   
+});
+
+// code for Day & Date
+
+const today = new Date();
+const day = today.toLocaleString('en-us', { weekday: 'short' });
+const formattedDate = today.toLocaleDateString('en-us',{month:'short',day:'numeric',year:'numeric'});
+document.getElementById("current-day").innerHTML = day + ',';
+document.getElementById("current-date").innerHTML = formattedDate;
